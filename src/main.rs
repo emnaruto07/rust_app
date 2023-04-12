ae5b58a7d919b3172f33c954f1af9fbd5b184fec
emnaruto07 <shazebvps@gmail.com>
2023-04-13 02:13:53 +0300

    Co-authored-by: Keogami <keogami@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,106 @@
 fn main() {
-    let my_var: usize = 1;
-    let my_int: isize = -1;
-    let floats: f32 = 1.9;
-    let double: f64 = 23.8;
-    let new_var = 8u8; // only with numbers
-    let with_underscore = 88_000; // == 88000
-    let character: char = 'k'; // single utf-8 encoded character
-    let string: &str = "adfsadfasfd"; // string slice
-    let boolean: bool = true; //  or false
-    println!("Hello, world!");
-    println!("my var is {}", my_var);
-    println!("my var is {my_var}");
+    let mut naruto: String = String::from("hello sarthak");
+    let mut sarthak: &mut String = &mut naruto;
+    //sarthak = Ox4456
+    //sarthak = Test
 
-    // let add5 = my_var + 5; // +, -, /, *, |, &, << everything
 
-    // let mut mutable_var = 3; // mutable, changeable
+    let sarthak: &str = "Test"; // variable shadowing;
 
-    // mutable_var = 5;
+    println!("{sarthak}");
+    // println!("{sarthak}");
+    println!("{naruto}");
+}
 
-    // let temperature: f32 = calculate_temp();
+// fn main() {
+//     let mut number = 5;
+//     let mut number2 = 9;
 
-    // // using temperature
+//     let mut my_ref = &mut number;
 
-    // temperature = temperature + 5
+//     *my_ref += 5;
 
-    // let if_var = if temperature == 1.2 {
+//     println!("number is {}", *my_ref);
 
-    // } else {
+//     my_ref = &mut number2;
+
+//     *my_ref += 3;
+
+//     println!("number2 is {}", *my_ref);
+
+//     // let mut number = 5;
+//     // let mut my_mut_ref: &mut u8 = &mut number; // read and write
+
+//     // let mut my_ref: &u8 = &number; 
+    
+
+
+
+
+
+
+//     // let number: u8  = 4;
+
+//     // let my_ref: &u8 = &number; // readonly
+
+//     // let calc = *my_ref + 3;
+//     // // *my_ref = 3; // invalid
+
+//     // let mut mut_number = 3;
+
+//     // let my_immut_ref = &mut_number; // readonly
+//     // // *my_mut_ref = 5; // change
+
+//     // *my_mut_ref = 3434; // invalid
+//     // my_mut_ref = &mut_number; // valid
+
+//     // // *my_immut_ref = 6;
+// }
+
+// // fn main() {
+// //     let my_var: usize = 1;
+// //     let my_int: isize = -1;
+// //     let floats: f32 = 1.9;
+// //     let double: f64 = 23.8;
+// //     let new_var = 8u8; // only with numbers
+// //     let with_underscore = 88_000; // == 88000
+// //     let character: char = 'k'; // single utf-8 encoded character
+// //     let string: &str = "adfsadfasfd"; // string slice
+// //     let boolean: bool = true; //  or false
+// //     println!("Hello, world!");
+// //     println!("my var is {}", my_var);
+// //     println!("my var is {my_var}");
+
+// //     // let add5 = my_var + 5; // +, -, /, *, |, &, << everything
+
+// //     // let mut mutable_var = 3; // mutable, changeable
+
+// //     // mutable_var = 5;
+
+// //     // let temperature: f32 = calculate_temp();
+
+// //     // // using temperature
+
+// //     // temperature = temperature + 5
+
+// //     // let if_var = if temperature == 1.2 {
+
+// //     // } else {
         
-    // }
+// //     // }
 
-    let cond_var = if my_var == 1 {
-        println!("eight");
-        8
-    } else {
-        println("ten");
-        // 10
-        "sadfasfd"
-    }
+// //     let cond_var = if my_var == 1 {
+// //         println!("eight");
+// //         8
+// //     } else {
+// //         println("ten");
+// //         // 10
+// //         "sadfasfd"
+// //     }
 
     
 
 
-}
+// // }
 
-//
+// // //
